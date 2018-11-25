620a49fc1880fef23dd76ec58177fc59f12f079a
plus-info-tech <44468047+plus-info-tech@users.noreply.github.com>
2018-11-25 23:56:32 +0900

    今度こそ前進！
    いったれー*\(^o^)/*

---
+++ app/sql/local.wp_terms.sql
@@ -0,0 +1,10 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_terms` VALUES
+(1,"Uncategorized","uncategorized",0),
+(2,"メインメニュー","%e3%83%a1%e3%82%a4%e3%83%b3%e3%83%a1%e3%83%8b%e3%83%a5%e3%83%bc",0),
+(3,"①EXCELコース","ex",0),
+(4,"②基礎力向上コース","base",0),
+(5,"③SE育成コース","se",0),
+(6,"④HP作成コース","hp",0);
---
+++ app/sql/local.wp_usermeta.sql
@@ -0,0 +1,30 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_usermeta` VALUES
+(1,1,"nickname","admin"),
+(2,1,"first_name",""),
+(3,1,"last_name",""),
+(4,1,"description",""),
+(5,1,"rich_editing","true"),
+(6,1,"syntax_highlighting","true"),
+(7,1,"comment_shortcuts","false"),
+(8,1,"admin_color","fresh"),
+(9,1,"use_ssl","0"),
+(10,1,"show_admin_bar_front","true"),
+(11,1,"locale",""),
+(12,1,"wp_capabilities","a:1:{s:13:\"administrator\";b:1;}"),
+(13,1,"wp_user_level","10"),
+(14,1,"dismissed_wp_pointers","wp496_privacy,theme_editor_notice"),
+(15,1,"show_welcome_panel","0"),
+(16,1,"session_tokens","a:1:{s:64:\"131485373d1dd6ea60d3b677d707ed0285b5824decca384c6183e1919e5a6b57\";a:4:{s:10:\"expiration\";i:1544366607;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:115:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36\";s:5:\"login\";i:1543157007;}}"),
+(17,1,"wp_dashboard_quick_press_last_post_id","57"),
+(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
+(19,1,"closedpostboxes_page","a:0:{}"),
+(20,1,"metaboxhidden_page","a:3:{i:0;s:10:\"postcustom\";i:1;s:16:\"commentstatusdiv\";i:2;s:9:\"authordiv\";}"),
+(21,1,"wp_user-settings","editor=html&libraryContent=browse"),
+(22,1,"wp_user-settings-time","1539868541"),
+(23,1,"managenav-menuscolumnshidden","a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}"),
+(24,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}"),
+(25,1,"nav_menu_recently_edited","2"),
+(26,1,"show_try_gutenberg_panel","0");
