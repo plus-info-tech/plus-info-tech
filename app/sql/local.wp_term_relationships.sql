dc5b40d12a09af09d43cf8608a1d09b0e947ede8
plus-info-tech <44468047+plus-info-tech@users.noreply.github.com>
2018-11-25 10:49:38 +0900

    追加しました
    さのっち追加

---
+++ app/sql/local.wp_term_relationships.sql
@@ -0,0 +1,15 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_term_relationships` VALUES
+(1,1,0),
+(17,2,0),
+(18,2,0),
+(20,2,0),
+(35,2,0),
+(36,2,0),
+(37,2,0),
+(38,2,0),
+(39,2,0),
+(47,3,0),
+(49,5,0);
