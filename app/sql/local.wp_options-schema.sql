5e6eee20a08867f11ff0b85e8da4619c20311dcb
plus-info-tech <44468047+plus-info-tech@users.noreply.github.com>
2018-11-27 14:36:04 +0900

    仮コミット

--- app/sql/local.wp_options-schema.sql
+++ app/sql/local.wp_options-schema.sql
@@ -9,4 +9,3 @@ CREATE TABLE `wp_options` (
   PRIMARY KEY (`option_id`),
   UNIQUE KEY `option_name` (`option_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=319 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
-) ENGINE=InnoDB AUTO_INCREMENT=319 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
