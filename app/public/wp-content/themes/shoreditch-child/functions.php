d2337e4a0fa3ad9c1a5504e8b3914f360dc1c372
plus-info-tech <44468047+plus-info-tech@users.noreply.github.com>
2018-11-27 23:53:09 +0900

    かとちゃナイズ！

--- app/public/wp-content/themes/shoreditch-child/functions.php
+++ app/public/wp-content/themes/shoreditch-child/functions.php
@@ -5,4 +5,61 @@ function theme_enqueue_styles() {
   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style')
 );
 }
+
+function bourgeon_create_post_type() {
+  register_post_type( 'seminar',
+    array(
+      'labels' => array(
+        'name'          => 'セミナー情報',
+        'singular_name' => 'セミナー情報',
+      ),
+      'public' => true,
+      'menu_position' => 5,
+      'has_archive'   => true,
+      'supports' => array(
+        'title',
+        'editor',
+        'excerpt',
+        'thumbnail',
+        'custom-fields',
+      ),
+    )
+  );
+
+  register_post_type( 'info',
+    array(
+      'labels' => array(
+        'name'          => 'お知らせ',
+        'singular_name' => 'お知らせ',
+      ),
+      'public'        => false,
+      'show_ui'       => true,
+      'menu_position' => 5,
+      'has_archive'   => true,
+      'supports' => array(
+        'title',
+        'editor',
+        'excerpt',
+      ),
+    )
+  );
+}
+add_action( 'init', 'bourgeon_create_post_type', 1 );
+
+
+function bourgeon_create_taxonomies() {
+  $labels = array(
+    'name'          => 'セミナー内容',
+    'singular_name' => 'セミナー内容',
+  );
+
+  register_taxonomy( 'target', array( 'seminar' ),
+    array(
+      'hierarchical' => false,
+      'labels'       => $labels,
+      'show_ui'      => true,
+    )
+  );
+}
+add_action( 'init', 'bourgeon_create_taxonomies', 0 );
 ?>
